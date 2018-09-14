bca616ec4923f2405667a36a85b766aeeb2f3181
Alexander Kondratyev <alex@nder.work>
2018-09-14 12:23:20 +0300

    Initial Commit

---
+++ naive/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  naive
+//
+//  Created by Alexandr Kondratyev on 14/09/2018.
+//  Copyright © 2018 alexander. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
